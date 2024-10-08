6968b11434d9b26e5ec024f56f8ce2fad0af4af6
Ahmad Ibrahim <ahmadmuhamo@gmail.com>
2024-10-09 02:23:06 +0300

    Dropdowns & APIs

--- pages/index.vue
+++ pages/index.vue
@@ -1,8 +1,71 @@
 <template>
   <v-row justify="center" align="center">
-    <v-col cols="12" sm="8" md="6">
-      <v-card class="logo py-4 d-flex justify-center">
-        <h1>Mazaady</h1>
+    <v-col cols="12" sm="10" md="8">
+      <v-card class="logo pa-4 d-flex justify-center">
+        <v-row>
+          <v-col cols="12">
+            <label>Main Category</label>
+            <v-autocomplete
+              v-model="category"
+              :loading="loadingCategories"
+              :items="categories"
+              placeholder="Select Main Category"
+              item-text="name"
+              return-object
+              outlined
+              hide-details
+            />
+          </v-col>
+          <v-col cols="12">
+            <label>Sub Category</label>
+            <v-autocomplete
+              v-model="subCategory"
+              :items="subCategories"
+              placeholder="Select Sub Category"
+              item-text="name"
+              return-object
+              outlined
+              hide-details
+              @update:modelValue="fetchCatProperties"
+            />
+          </v-col>
+          <v-col v-if="loadingProperties" cols="12">
+            <v-skeleton-loader type="list-item, list-item, list-item" />
+          </v-col>
+          <template v-else-if="catProperties.length">
+            <v-col v-for="prop in catProperties" :key="prop.id" cols="12">
+              <label>{{ prop.name }}</label>
+              <v-text-field
+                v-if="!prop.options.length"
+                v-model="properties[prop.slug]"
+                outlined
+                hide-details
+              ></v-text-field>
+              <template v-else>
+                <v-autocomplete
+                  v-model="properties[prop.slug]"
+                  :items="prop.options"
+                  placeholder="Select Option"
+                  item-text="name"
+                  return-object
+                  outlined
+                  hide-details
+                />
+                <v-autocomplete
+                  v-if="childProps[prop.slug]?.length"
+                  v-model="childProperties[prop.slug]"
+                  class="mt-2"
+                  :items="childProps[prop.slug]"
+                  placeholder="Select Option"
+                  item-text="name"
+                  return-object
+                  outlined
+                  hide-details
+                />
+              </template>
+            </v-col>
+          </template>
+        </v-row>
       </v-card>
     </v-col>
   </v-row>
@@ -11,8 +74,69 @@
 <script>
 export default {
   name: 'IndexPage',
-  mounted() {
-    this.$store.dispatch('fetchCategories')
+  data() {
+    return {
+      category: {},
+      subCategory: {},
+      properties: {},
+      childProperties: {},
+      childProps: {},
+      subCategories: [],
+      test: {},
+      loadingCategories: true,
+      loadingProperties: false
+    }
+  },
+  computed: {
+    categories() {
+      return this.$store.getters.GET_CATEGORIES
+    },
+    catProperties: {
+      get() {
+        return this.$store.getters.GET_CATPROPS
+      },
+      set(val) {
+        this.$store.commit('SET_CATPROPS', val)
+      }
+    }
+  },
+  watch: {
+    category: {
+      deep: true,
+      handler(cat) {
+        this.subCategories = cat.children
+        this.subCategory = {}
+        this.catProperties = {}
+      }
+    },
+    subCategory() {
+      this.fetchCatProperties()
+    },
+    properties: {
+      deep: true,
+      async handler(val) {
+        for (const key in val) {
+          if (val[key].child) {
+            this.childProps[key] = await this.$store.dispatch('fetchChildProps', val[key].id)
+            this.$forceUpdate()
+          }
+        }
+      }
+    }
+  },
+  async created() {
+    this.loadingCategories = true
+    await this.$store.dispatch('fetchCategories')
+    this.loadingCategories = false
+  },
+  methods: {
+    async fetchCatProperties() {
+      this.loadingProperties = true
+      this.properties = {}
+      this.childProperties = {}
+      await this.$store.dispatch('fetchCatProperties', this.subCategory.id)
+      this.loadingProperties = false
+    }
   }
 }
 </script>
--- store/index.js
+++ store/index.js
@@ -1,21 +1,27 @@
 export const state = () => ({
   categories: [],
-  subCategories: [],
-  allOptions: []
+  catProperties: []
 })
 
 export const mutations = {
   SET_CATEGORIES(state, categories) {
     state.categories = categories
   },
-  SET_SUBCATEGORIES(state, subCategories) {
-    state.subCategories = subCategories
+  SET_PROPERTIES(state, propterties) {
+    state.catProperties = propterties
   },
-  SET_OPTIONS(state, allOptions) {
-    state.allOptions = allOptions
+  SET_CATPROPS(state, properties) {
+    state.catProperties = properties
   }
 }
-
+export const getters = {
+  GET_CATEGORIES(state) {
+    return state.categories
+  },
+  GET_CATPROPS(state) {
+    return state.catProperties
+  }
+}
 export const actions = {
   async fetchCategories({ commit }) {
     const apiKey = this.$config.apiKey
@@ -24,6 +30,30 @@ export const actions = {
         headers: { 'private-key': apiKey }
       })
       commit('SET_CATEGORIES', res.data.data.categories)
+    } catch (error) {
+      // eslint-disable-next-line no-console
+      console.error('Error trying to fetch categories.', error.message)
+    }
+  },
+  async fetchCatProperties({ commit }, id) {
+    const apiKey = this.$config.apiKey
+    try {
+      const res = await this.$axios.get(`properties?cat=${id}`, {
+        headers: { 'private-key': apiKey }
+      })
+      commit('SET_PROPERTIES', res.data.data)
+    } catch (error) {
+      // eslint-disable-next-line no-console
+      console.error('Error trying to fetch categories.', error.message)
+    }
+  },
+  async fetchChildProps({ commit }, id) {
+    const apiKey = this.$config.apiKey
+    try {
+      const res = await this.$axios.get(`get-options-child/${id}`, {
+        headers: { 'private-key': apiKey }
+      })
+      return res.data.data[0].options
     } catch (error) {
       // eslint-disable-next-line no-console
       console.error('Error trying to fetch categories.', error.message)
